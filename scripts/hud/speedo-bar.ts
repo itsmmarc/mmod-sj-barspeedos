1814d79d0d7ab795fa6343183224f06531c5a772
itsmmarc <officialmarcusmifsud@gmail.com>
2026-07-30 21:13:52 +1000

    mostly functional port to hud customizer - vertical bar currently resizes to width instead of height - no implementation of speedo color ranges yet

--- scripts/hud/speedo-bar.ts
+++ scripts/hud/speedo-bar.ts
@@ -1,9 +1,9 @@
+import { CustomizerPropertyType, registerHUDCustomizerComponent } from 'common/hud-customizer';
 import { SpeedometerColorType, SpeedometerType } from 'common/speedometer';
-import { GamemodeCategory } from 'common/web/enums/gamemode.enum';
+import { GamemodeCategory, GamemodeCategoryToGamemode } from 'common/web/enums/gamemode.enum';
 import { GamemodeCategories } from 'common/web/maps/gamemodes.map';
 import { RgbaTuple, rgbaStringToTuple, tupleToRgbaString } from 'util/colors';
-import { PanelHandler } from 'util/module-helpers';
-import { RegisterHUDPanelForGamemode } from 'util/register-for-gamemodes';
+import { OnPanelLoad, PanelHandler } from 'util/module-helpers';
 
 // copied from `hud/speedometer.ts`, should probably be imported from a util file instead
 interface Range {
@@ -14,7 +14,7 @@ interface Range {
 type RuntimeSettings = SpeedometerSettingsAPI.Settings & { range_colors?: Range[] };
 
 // settings are temporarily hardcoded, should be replaced with an api call later
-const overlays = {
+const markers = {
 	horizontal: [855, 895],
 	vertical: [450, 1100]
 };
@@ -58,7 +58,7 @@ const verticalSettings: RuntimeSettings = {
 };
 
 @PanelHandler()
-class SpeedoBarHandler {
+class SpeedoBarHandler implements OnPanelLoad {
 	readonly panels = {
 		cp: $.GetContextPanel<MomHudSpeedoBars>(),
 		horizontalMeter: $<ProgressBar>('#HorizontalSpeedoBarMeter'),
@@ -66,19 +66,120 @@ class SpeedoBarHandler {
 	};
 
 	constructor() {
-		RegisterHUDPanelForGamemode({
-			gamemodes: GamemodeCategories.get(GamemodeCategory.SJ),
-			onLoad: () => this.createOverlays(),
-			events: [
-				{
-					event: 'OnSpeedometerUpdate',
-					callback: () => this.onSpeedoBarsUpdate()
+		$.RegisterForUnhandledEvent('OnSpeedometerUpdate', () => this.onSpeedoBarsUpdate());
+		registerHUDCustomizerComponent($.GetContextPanel(), {
+			name: 'Speedo Bars', // #LOCALIZE_ME
+			resizeX: true,
+			resizeY: true,
+			gamemode: GamemodeCategoryToGamemode.get(GamemodeCategory.SJ),
+			dynamicStyles: {
+				borderStyles: {
+					name: $.Localize('#Customizer_Sticky_Charge_BorderStyles'),
+					type: CustomizerPropertyType.NONE,
+					expandable: true,
+					children: [{ styleID: 'borderWidth' }, { styleID: 'borderColor' }, { styleID: 'borderRadius' }]
+				},
+				borderWidth: {
+					name: $.Localize('#Customizer_BorderWidth'),
+					type: CustomizerPropertyType.NUMBER_ENTRY,
+					targetPanel: '.speedobar__meter',
+					styleProperty: 'borderWidth',
+					valueFn: (value) => `${value}px`
+				},
+				borderColor: {
+					name: $.Localize('#Customizer_BorderColor'),
+					type: CustomizerPropertyType.COLOR_PICKER,
+					targetPanel: '.speedobar__meter',
+					styleProperty: 'borderColor'
+				},
+				borderRadius: {
+					name: $.Localize('#Customizer_BorderRadius'),
+					type: CustomizerPropertyType.NONE,
+					expandable: true,
+					children: [{ styleID: 'backgroundRadius' }, { styleID: 'fillRadius' }]
+				},
+				backgroundRadius: {
+					name: $.Localize('#Customizer_Background'),
+					type: CustomizerPropertyType.NUMBER_ENTRY,
+					targetPanel: '.speedobar__meter',
+					styleProperty: 'borderRadius',
+					valueFn: (value) => `${value}px`,
+					settingProps: { min: 0, max: 10 }
+				},
+				fillRadius: {
+					name: $.Localize('#Customizer_Fill'),
+					type: CustomizerPropertyType.NUMBER_ENTRY,
+					targetPanel: '.ProgressBarLeft',
+					styleProperty: 'borderRadius',
+					valueFn: (value) => `${value}px`,
+					settingProps: { min: 0, max: 10 }
+				},
+				colors: {
+					name: $.Localize('#Customizer_Colors'),
+					type: CustomizerPropertyType.NONE,
+					expandable: true,
+					children: [
+						{ styleID: 'backgroundGradient' },
+						{ styleID: 'fillGradient' },
+						{ styleID: 'markerColor' }
+					]
+				},
+				backgroundGradient: {
+					name: $.Localize('#Customizer_Background'),
+					type: CustomizerPropertyType.GRADIENT_PICKER,
+					targetPanel: '.ProgressBarRight',
+					styleProperty: 'backgroundColor',
+					valueFn: (value) => {
+						return `gradient(linear, 0% 0%, 100% 0%, from (${value[0]}), to(${value[1]}))` as color;
+					}
+				},
+				fillGradient: {
+					name: $.Localize('#Customizer_Fill'),
+					type: CustomizerPropertyType.GRADIENT_PICKER,
+					targetPanel: '.ProgressBarLeft',
+					styleProperty: 'backgroundColor',
+					valueFn: (value) => {
+						return `gradient(linear, 0% 0%, 100% 0%, from (${value[0]}), to(${value[1]}))` as color;
+					}
+				},
+				markerColor: {
+					name: 'Marker Color', // #LOCALIZE_ME
+					type: CustomizerPropertyType.COLOR_PICKER,
+					targetPanel: '.speedobar__marker',
+					styleProperty: 'backgroundColor'
 				}
-			]
+			},
+			postInit: () => {
+				this.createMarkers();
+			}
 		});
 	}
 
+	onPanelLoad() {
+		this.createMarkers();
+	}
+
+	createMarkers() {
+		// create markers
+		for (const speed of markers.horizontal) {
+			let speedPercentage = (speed / maxSpeed.horizontal) * 100;
+			const newPanel = $.CreatePanel('Panel', this.panels.cp, '', {
+				class: `speedobar__marker speedobar__marker__horizontal`
+			});
+			newPanel.style.position = `${speedPercentage}% 0 0`;
+		}
+		for (const speed of markers.vertical) {
+			let speedPercentage = (speed / maxSpeed.vertical) * 100;
+			const newPanel = $.CreatePanel('Panel', this.panels.cp, '', {
+				class: `speedobar__marker speedobar__marker__vertical`
+			});
+			newPanel.style.position = `0 ${-speedPercentage}% 0`;
+		}
+	}
+
 	onSpeedoBarsUpdate() {
+		// set vertical meter width to container height
+		$('#VerticalSpeedoBarMeter').style.width = `${this.panels.cp.actuallayoutheight}px`;
 		const velocity = MomentumPlayerAPI.GetVelocity();
 		this.updateMeter(this.panels.horizontalMeter, velocity, horizontalSettings, maxSpeed.horizontal);
 		this.updateMeter(this.panels.verticalMeter, velocity, verticalSettings, maxSpeed.vertical);
@@ -89,48 +190,35 @@ class SpeedoBarHandler {
 		const speedPercentage = speed / maxSpeed;
 		panel.value = speedPercentage;
 
-		this.colorMeter(panel, speed, settings);
+		// this.colorMeter(panel, speed, settings);
 	}
 
-	colorMeter(panel: ProgressBar, speed: number, settings: RuntimeSettings) {
-		let progressBarLeft = panel.FindChildrenWithClassTraverse('ProgressBarLeft');
+	// colorMeter(panel: ProgressBar, speed: number, settings: RuntimeSettings) {
+	// 	let progressBarLeft = panel.FindChildrenWithClassTraverse('ProgressBarLeft');
 
-		if (settings.color_type === SpeedometerColorType.RANGE && settings.range_colors) {
-			for (const range of settings.range_colors) {
-				if (speed >= range.min && speed <= range.max) {
-					// set highlight color to a lighter shade of range color
-					let highlightColorTuple: RgbaTuple = rgbaStringToTuple(range.color);
-					for (let i = 0; i < highlightColorTuple.length - 1; i++) {
-						highlightColorTuple[i] += 75;
-						if (highlightColorTuple[i] > 255) {
-							highlightColorTuple[i] = 255;
-						}
-					}
-					let highlightColor: rgbaColor = tupleToRgbaString(highlightColorTuple);
+	// 	if (settings.color_type === SpeedometerColorType.RANGE && settings.range_colors) {
+	// 		for (const range of settings.range_colors) {
+	// 			if (speed >= range.min && speed <= range.max) {
+	// 				// set highlight color to a lighter shade of range color
+	// 				let highlightColorTuple: RgbaTuple = rgbaStringToTuple(range.color);
+	// 				for (let i = 0; i < highlightColorTuple.length - 1; i++) {
+	// 					highlightColorTuple[i] += 75;
+	// 					if (highlightColorTuple[i] > 255) {
+	// 						highlightColorTuple[i] = 255;
+	// 					}
+	// 				}
+	// 				let highlightColor: rgbaColor = tupleToRgbaString(highlightColorTuple);
 
-					progressBarLeft[0].style.backgroundColor =
-						'gradient(linear, 0% 0%, 100% 0%, from(' +
-						range.color +
-						'), to(' +
-						highlightColor +
-						')) !default';
-				}
-			}
-		}
-	}
-
-	createOverlays() {
-		for (const speed of overlays.horizontal) {
-			let speedPercentage = (speed / maxSpeed.horizontal) * 100;
-			const newPanel = $.CreatePanel('Panel', this.panels.cp, '', { class: `speedobar__overlay__horizontal` });
-			newPanel.style.position = `${speedPercentage}% 0 0`;
-		}
-		for (const speed of overlays.vertical) {
-			let speedPercentage = (speed / maxSpeed.vertical) * 100;
-			const newPanel = $.CreatePanel('Panel', this.panels.cp, '', { class: `speedobar__overlay__vertical` });
-			newPanel.style.position = `0 ${-speedPercentage}% 0`;
-		}
-	}
+	// 				progressBarLeft[0].style.backgroundColor =
+	// 					'gradient(linear, 0% 0%, 100% 0%, from(' +
+	// 					range.color +
+	// 					'), to(' +
+	// 					highlightColor +
+	// 					')) !default';
+	// 			}
+	// 		}
+	// 	}
+	// }
 
 	// copied from `hud/speedometer.ts`, should probably be imported from a util file instead
 	getSpeedFromVelocity({ x, y, z }: vec3, settings: SpeedometerSettingsAPI.Settings): float {
